80100b195820a15138d30eada485f92929d9d828
DanBeverley <hoap43431@gmail.com>
2025-06-09 08:38:49 +0700

    add crud.py : handle database operations

---
+++ ai_job_search_app/backend/crud.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Session
+
+from .models.db import user as model
+from . import schemas
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(model.User).filter(model.User.email == email).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate, hashed_password: str):
+    db_user = model.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user 
